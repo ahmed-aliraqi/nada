299417a73352f458d54cb3a1470ac25be9482ec0
Ahmed Fathy <fathy.dev@gmail.com>
2026-04-20 10:07:21 +0300

    Integrate Tawk.to script in app layout
    Added Tawk.to live chat script to the layout.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
     <link href="/v3/css/style-{{ Locales::getDir() }}.css" rel="stylesheet">
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/69e5ce8c49bb761c347ce0c3/1jmkqtldl';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+<!--End of Tawk.to Script-->
 </head>
 <body>
 <!-- Loading Screen -->
